5a7c6f332288441b163a7cdd946c898b79d8d1f7
Louya64 <cixie@live.fr>
2021-09-01 15:51:57 +0200

    fonctionnalité changement de noms + ajout commentaires

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-let cellules = document.getElementsByClassName("case");
-let joueur = document.getElementById("joueur");
+
+let formNoms = document.getElementById("formNoms");
 let plateau = document.getElementById("plateau");
+let cellules = document.getElementsByClassName("case");
 let croix = '<img src="images/croix.svg" alt="croix" width="195">';
 let rond = '<img src="images/rond.svg" alt="rond" width="150">';
-let winner = document.getElementById("winner");
-let declarationWinner = document.getElementById("declarationWinner");
+let joueur = document.getElementById("joueur");
 let tourJoueur = document.getElementById("tourJoueur");
 let nomsJoueurs = document.getElementById("nomsJoueurs");
-let joueur1 = 'Joueur 1';
-let joueur2 = 'Joueur 2';
-let player = joueur1;
+let joueur1 = sessionStorage.getItem('joueur1') || 'Joueur 1';
+let joueur2 = sessionStorage.getItem('joueur2') || 'Joueur 2';
+let player = joueur1; //initialisation du joueur dont c'est le tour (joueur 1 en début de partie)
+let declarationWinner = document.getElementById("declarationWinner");
+let winner = document.getElementById("winner");
 let btnRecommencer = document.getElementById("recommencer");
-let formNoms = document.getElementById("formNoms");
 
-formNoms.classList.add('d-none');
-declarationWinner.classList.add('d-none');
-tourJoueur.classList.remove('d-none');
-joueur.innerHTML = player; //affiche le tour du joueur dont c'est le tour
+formNoms.classList.add('d-none'); //ne s'affiche que pour changer de noms
+declarationWinner.classList.add('d-none');//ne s'affiche que quand la partie est finie (gagnant ou null)
+tourJoueur.classList.remove('d-none');//ne s'affiche que quand la partie est en cours
+joueur.innerHTML = player; //affichage nom joueur en cours
 
+//changement de noms (peut se faire à tout moment)
 nomsJoueurs.addEventListener("click", function() {
     formNoms.classList.remove('d-none');
     formNoms.addEventListener("submit", function(e) {
         e.preventDefault();
         let ancienNom1 = joueur1;
-        // let ancienNom2 = joueur2;
-        joueur1 = document.getElementById("joueur1").value;
-        joueur2 = document.getElementById("joueur2").value;
+        if (document.getElementById("joueur1").value != '') {
+            sessionStorage.setItem('joueur1', document.getElementById("joueur1").value); //stckage dans variable session pour garder valeur au reload (d'une partie à l'autre)
+            joueur1 = document.getElementById("joueur1").value;
+        }
+        if (document.getElementById("joueur2").value != '') {
+            sessionStorage.setItem('joueur2', document.getElementById("joueur2").value);
+            joueur2 = document.getElementById("joueur2").value;
+        }
 
-        ///si champ vide, remettre ancien nom
-
+        //mise à jour variable player
         if (player == ancienNom1) {
             player = joueur1;
         } else {
             player = joueur2;
         }
         joueur.innerHTML = player;
-        console.log(joueur1)
-        console.log(player)
+        formNoms.classList.add('d-none');
     })
-    // player = player;
-    
 })
 
+//récup case choisie par joueur et insère rond ou croix selon joueur puis désactive la case (pour pas réecrire dessus)
 for (let cellule of cellules) {
-        cellule.addEventListener("click", function(){
+    cellule.addEventListener("click", function(){
         if (declarationWinner.classList.contains("d-none")) {
             if (player === joueur1) {
                 cellule.innerHTML = croix;
@@ -57,10 +61,7 @@ for (let cellule of cellules) {
     }, {once : true})
 }
 
-btnRecommencer.addEventListener("click", function() {
-    window.location.reload();
-})
-
+//vérif si y'a un winner
 plateau.addEventListener("click", function() {
     let case0 = cellules[0].innerHTML
     let case1 = cellules[1].innerHTML
@@ -102,9 +103,15 @@ plateau.addEventListener("click", function() {
         winner.innerHTML = joueur2;
     }
 
+    //si toutes les cases sont remplies sans gagnant, c'est perdu!
     if (case0 != '' && case1 != '' && case2 != '' && case3 != '' && case4 != '' && case5 != '' && case6 != '' && case7 != '' && case8 != '') {
         tourJoueur.classList.add('d-none');
         declarationWinner.classList.remove('d-none');
         winner.innerHTML = "Personne n'";
     }
+})
+
+//btn recommencer -> reload page
+btnRecommencer.addEventListener("click", function() {
+    window.location.reload();
 })
